3b1711d8bff44432abc5aa5d0663abbcaba6987e
deboo <deboo@HUSKER>
2025-02-05 21:52:54 +0200

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введiть кiлькiсть хвилин: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("");
+Console.Write("В годинах це: ");
+int b = (a / 60);
+Console.WriteLine(b);
+Console.Write("");
+Console.Write("Хвилин: ");
+int c = (a % 60);
+Console.Write(c);
+    
+
